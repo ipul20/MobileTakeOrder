438c333eead94c784f1267a5bbacdd7b8ae31050
Ahmad Syaiful Yusuf <ipul2006000@gmail.com>
2023-06-22 09:25:31 +0800

    create env file

---
+++ env.js
@@ -0,0 +1,7 @@
+export const BASE_URL = 'https://order.portalabsen.com';
+// export const BASE_URL = 'http://117.53.44.58';
+export const API_BASE_URL = BASE_URL + '/api';
+
+export const DEFAULT_PROFILE_PICTURE = {
+  uri: 'https://cdn2.iconfinder.com/data/icons/avatars-99/62/avatar-372-456324-128.png',
+};
