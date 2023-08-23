125eb296fc93d34a6e35172050d4275d755809b6
Ahmad Syaiful Yusuf <ipul2006000@gmail.com>
2023-08-23 16:22:16 +0800

    update Riwayat Proses User

--- src/screens/main/Riwayat.js
+++ src/screens/main/Riwayat.js
@@ -1,15 +1,52 @@
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+
 import {View, Text} from 'react-native';
 import RiwayatProses from '../page/RiwayatProses';
 import RiwayatSelesai from '../page/RiwayatSelesai';
+export default function Riwayat({navigation}) {
+  const [reload, setReload] = useState(1);
+  const [riwayat, setRiwayat] = useState({
+    selesai: [],
+    belum: [],
+  });
+  const getRiwayat = async () => {
+    try {
+      const response = await fetch(
+        'https://order.portalabsen.com/api/riwayat-pesanan/1',
+      );
+      const json = await response.json();
+      console.log(json.data.selesai);
+      setRiwayat({
+        selesai: json.data.selesai,
+        belum: json.data.belum,
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('focus', () => {
+      getRiwayat();
+      //Put your Data loading function here instead of my loadData()
+    });
 
-export default function Riwayat() {
+    getRiwayat();
+    return unsubscribe;
+  }, [reload, navigation]);
   const Tab = createMaterialTopTabNavigator();
   return (
     <>
       <Tab.Navigator>
-        <Tab.Screen name="Proses" component={RiwayatProses} />
+        <Tab.Screen
+          name="Proses"
+          children={() => (
+            <RiwayatProses
+              data={riwayat.belum}
+              reloadPage={() => setReload(reload + 1)}
+            />
+          )}
+        />
         <Tab.Screen name="Selesai" component={RiwayatSelesai} />
       </Tab.Navigator>
     </>
--- src/screens/page/RiwayatProses.js
+++ src/screens/page/RiwayatProses.js
@@ -1,10 +1,88 @@
-import React from 'react';
-import {View, Text} from 'react-native';
-
-export default function RiwayatProses() {
+import React, {useState} from 'react';
+import {View, Text, ScrollView, Image, TouchableOpacity} from 'react-native';
+import {Button, IconButton, MD3Colors} from 'react-native-paper';
+import {
+  heightPercentageToDP as hp,
+  widthPercentageToDP as wp,
+} from 'react-native-responsive-screen';
+import {API_BASE_URL, BASE_URL} from '../../../env';
+import {COLOR, COLOR_GRAY} from '../../styles';
+export default function RiwayatProses(props) {
+  const data = props.data ?? [];
+  console.log(data);
   return (
     <View>
-      <Text>PROSES</Text>
+      <ScrollView
+        contentContainerStyle={{
+          minHeight: hp(100),
+          paddingBottom: hp(25),
+        }}>
+        {data.map(v => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate('DetailTakeaway')}
+            style={{
+              flexDirection: 'row',
+              paddingVertical: wp(2),
+              paddingHorizontal: wp(4),
+              borderBottomColor: COLOR_GRAY.LIGHT,
+              borderBottomWidth: 1,
+              width: ' 100%',
+              justifyContent: 'space-between',
+            }}>
+            <View
+              style={{
+                flexDirection: 'row',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}>
+              <View
+                style={{
+                  marginTop: wp(2),
+                  marginRight: wp(3),
+                  padding: wp(1),
+                }}>
+                <Image
+                  source={require('../../assets/image/user.png')}
+                  style={{
+                    width: wp(20),
+                    height: wp(20),
+                    borderRadius: wp(1),
+                  }}
+                />
+              </View>
+              <View
+                style={{
+                  flexDirection: 'column',
+                }}>
+                <View
+                  style={{
+                    marginRight: wp(2),
+                  }}>
+                  <Text style={{fontSize: wp(5), fontWeight: 'bold'}}>
+                    Mie Pangsit
+                  </Text>
+                </View>
+                <View>
+                  <Text style={{fontSize: wp(3), fontWeight: 'bold'}}>
+                    Jenis Pesanan = {v.jenis}
+                  </Text>
+                </View>
+                <View style={{flexDirection: 'row'}}>
+                  <Text style={{fontSize: wp(3)}}>Status = </Text>
+                  <Text style={{fontSize: wp(3)}}>Status</Text>
+                </View>
+              </View>
+            </View>
+            <View
+              style={{
+                alignItems: 'center',
+                width: wp(31),
+                alignSelf: 'center',
+                flexDirection: 'row',
+              }}></View>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
   );
 }
